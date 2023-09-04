4c364ace6460aea967365cd9c099f2d5539194b5
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-04 19:42:49 +0300

    dev created serializers

---
+++ service/serializers.py
@@ -0,0 +1,26 @@
+"""Serializers for 'service' app."""
+from rest_framework import serializers
+
+from .models import Check, Printer
+
+
+class PrinterSerializer(serializers.ModelSerializer):
+    """Printer model serializer."""
+
+    class Meta:
+        """Class Meta for Printer serializer class."""
+
+        model = Printer
+        fields = "__all__"
+        read_only_fields = ("id",)
+
+
+class CheckSerializer(serializers.ModelSerializer):
+    """Check model serializer."""
+
+    class Meta:
+        """Class Meta for Check serializer class."""
+
+        model = Check
+        fields = "__all__"
+        read_only_fields = ("id",)
