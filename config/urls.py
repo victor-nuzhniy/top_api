2425eff917daa486a962179c4290606d11b3363b
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-05 21:15:26 +0300

    dev added swagger urls to config.urls

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Servise API",
+        default_version="v1",
+        description="Service for check creation and downloading.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="nuzhniyva@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("service/", include("service.urls")),
     path("", include_docs_urls(title="Zhrachka top service API")),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
