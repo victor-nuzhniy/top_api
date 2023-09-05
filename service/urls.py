5776b696c40ddb4a22a7668d7a3d1b89fa13a460
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-05 15:14:08 +0300

    dev added service urls

--- service/urls.py
+++ service/urls.py
@@ -1,8 +1,10 @@
 """'Service' app urls."""
 from django.urls import path
 
-from service.views import CheckCreateView
+from service.views import CheckPrinterView, CheckView, DownloadCheckView
 
 urlpatterns = [
-    path("check/", CheckCreateView.as_view(), name="create_check"),
+    path("check/", CheckView.as_view(), name="create_check"),
+    path("download/<int:pk>/", DownloadCheckView.as_view(), name="download_check"),
+    path("printer/<slug:api_key>/", CheckPrinterView.as_view(), name="rendered_checks"),
 ]
