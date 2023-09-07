93883bf1d93aa50750644d7b517c9cd5f60c6dce
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-07 09:42:19 +0300

    dev refactor celery settings placement

--- config/celeryy.py
+++ config/celeryy.py
@@ -9,9 +9,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery(
     "config",
-    broker="redis://127.0.0.1:6379",
-    backend="redis://127.0.0.1:6379",
-    include=["service.tasks"],
 )
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
