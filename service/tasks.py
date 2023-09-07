65d867171d60e35643cfda2b1b6208d43c49e8ec
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-07 09:40:47 +0300

    dev added celery task to tasks

--- service/tasks.py
+++ service/tasks.py
@@ -1,1 +1,14 @@
 """Module for celery tasks for 'service' app."""
+from __future__ import absolute_import, unicode_literals
+
+from typing import Dict
+
+from celery import shared_task
+
+from service.app_services.file_handlers import write_file
+
+
+@shared_task()
+def perform_file_writing(context: Dict, check_id: int, check_type: str) -> None:
+    """Perform write file operation."""
+    write_file(context, check_id, check_type)
