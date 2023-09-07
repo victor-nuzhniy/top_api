b08c945bfd57fca4c0151373cc85007d99ff4b49
victro-nuzhniy <nuzhniyva@gmail.com>
2023-09-07 09:41:46 +0300

    dev deleted write file logic (transferred to file_handlers), added celery task

--- service/app_services/utils.py
+++ service/app_services/utils.py
@@ -1,18 +1,14 @@
 """Utilities for 'service' app."""
-import base64
-import json
 from typing import Dict, Optional
 
-import requests
-from django.core.files.base import ContentFile
 from django.db.models import QuerySet
-from django.template.loader import render_to_string
 from rest_framework import status
 from rest_framework.serializers import ModelSerializer
 
 from service.app_services.exceptions import CustomValidationError
 from service.models import Check, Printer
 from service.serializers import CheckSerializer
+from service.tasks import perform_file_writing
 
 
 def get_printers(data: Dict) -> Optional[QuerySet]:
@@ -52,8 +48,9 @@ def perform_check_creation(data: Dict, printers: QuerySet) -> None:
     serializer.is_valid(raise_exception=True)
     for printer in printers:
         serializer.save(printer_id=printer, type=printer.check_type)
-        # TODO here must be inserted celery task call for pdf file creation
-        write_file(data.get("order"), serializer.instance.pk, printer.check_type)
+        perform_file_writing.delay(
+            data.get("order"), serializer.instance.pk, printer.check_type
+        )
         serializer.instance = None
 
 
@@ -97,40 +94,3 @@ def get_check_instance(data: Dict) -> Check:
             status_code=status.HTTP_404_NOT_FOUND,
         )
     return instance
-
-
-def get_pdf_file_name(order_id: int, check_type: str) -> str:
-    """Get check pdf file name."""
-    return f"{order_id}_{check_type}.pdf"
-
-
-def create_pdf_data(context: Dict, check_type: str) -> bytes:
-    """Create pdf data, using context dict, templates, wkhtmltopdf server."""
-    url = "http://127.0.0.1:8001/"
-    headers = {
-        "Content-Type": "application/json",
-    }
-    template: str = "client.html" if check_type == "client" else "kitchen.html"
-    content = render_to_string(template, context)
-
-    base64_bytes = base64.b64encode(bytes(content, "utf-8"))
-    base64_string = base64_bytes.decode("utf-8")
-
-    data = {"contents": base64_string}
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    return response.content
-
-
-def modify_check_instance(content: bytes, check_id: int, pdf_file: str) -> None:
-    """Modify check instance, add created pdf file, change status to 'rendered'."""
-    check = Check.objects.get(id=check_id)
-    check.pdf_file.save(pdf_file, ContentFile(content))
-    check.status = "rendered"
-    check.save()
-
-
-def write_file(context: Dict, check_id: int, check_type: str) -> None:
-    """Write pdf file, using check order data, attach it to check model."""
-    pdf_file: str = get_pdf_file_name(context.get("id"), check_type)
-    content: bytes = create_pdf_data(context, check_type)
-    modify_check_instance(content, check_id, pdf_file)
